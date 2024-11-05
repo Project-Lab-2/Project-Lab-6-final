2a4ea20901fa76c8fb902e94d6290214b4e5522f
PreethiMasina <masina.4@osu.edu>
2024-11-05 18:00:11 -0500

    part 2

--- Project-Lab-4-API-2.Data/Class1.cs
+++
@@ -1,6 +0,0 @@
-﻿namespace Project_Lab_4_API_2.Data;
-
-public class Class1
-{
-
-}
---
+++ Project-Lab-4-API-2.Data/StoreContext.cs
@@ -0,0 +1,15 @@
+﻿using Project.Lab4.API2.Domain.Catalog; 
+using Microsoft.EntityFrameworkCore;
+
+namespace Project.Lab4.API2.Data
+{
+    public class StoreContext : DbContext
+    {
+        public StoreContext(DbContextOptions<StoreContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Item> Items { get; set; }
+    }
+}
