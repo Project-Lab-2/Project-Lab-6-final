da5cb4f23dbb06dbc04f011f2ca96403a43dd7b0
Ahlam Abdulkadir <abdulkadir.28@osu.edu>
2024-11-14 17:39:11 -0500

    Add frontend files including .gitignore

---
+++ frontend/src/App.test.js
@@ -0,0 +1,23 @@
+import { render, screen } from '@testing-library/react';
+import App from './App';
+
+describe('The main app', () => {
+  it('should render the Orders link', () => {
+    render(<App />);
+    expect(screen.getByText('Orders')).toBeInTheDocument();
+  });
+
+  it('should render the Payments link', () => {
+    render(<App />);
+    expect(screen.getByText('Payments')).toBeInTheDocument();
+  });
+
+  it('should render the Home link', () => {
+    render(<App />);
+    expect(screen.getByRole('link', { name: 'Home' })).toBeInTheDocument();
+  });
+  it('should render the Home header', () => {
+    render(<App />);
+    expect(screen.getByRole('heading', { level: 2 })).toBeInTheDocument();
+  });
+});
